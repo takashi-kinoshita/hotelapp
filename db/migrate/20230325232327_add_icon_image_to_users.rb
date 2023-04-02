6dfd4763c04a913d430b922663d75cae4a79e618
takashi-kinoshita <121014620+takashi-kinoshita@users.noreply.github.com>
2023-04-03 04:59:44 +0900

    マイグレーションエラー修正

--- db/migrate/20230325232327_add_icon_image_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddIconImageToUsers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :icon_image, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -66,16 +66,17 @@ ActiveRecord::Schema.define(version: 2023_03_26_042142) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "self_introduction"
+    t.string "icon_image"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
-    t.string "icon_image"
-    t.text "self_introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
